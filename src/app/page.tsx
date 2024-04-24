9e57f659bf0eb30716161c84a34152a622c2dfe8
rjunio98 <robledorjunior1@gmail.com>
2024-04-24 16:03:06 -0300

    add component service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 import React from "react";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <HowItWorks />
       <FeaturedProperties />
+      <Services />
     </>
   );
 };
