e6933ee9086000220df71dd41ffe5cdab2357299
rjunio98 <robledorjunior1@gmail.com>
2024-04-24 13:32:45 -0300

    add component howitworks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <HowItWorks />
     </>
-  )
+  );
 };
 
 export default Home;
